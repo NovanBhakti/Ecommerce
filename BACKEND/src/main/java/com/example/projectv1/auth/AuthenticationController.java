b667634cc6ff9316e494532c225eb0717df959d8
Novan Nur Bhakti <novan.bhakti@binus.ac.id>
2023-11-15 13:25:51 +0700

    backend CORS

--- BACKEND/src/main/java/com/example/projectv1/auth/AuthenticationController.java
+++ BACKEND/src/main/java/com/example/projectv1/auth/AuthenticationController.java
@@ -1,14 +1,15 @@
 package com.example.projectv1.auth;
 
+import com.example.projectv1.user.UserResponse;
+import com.example.projectv1.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin
 @RequiredArgsConstructor
 public class AuthenticationController {
 
@@ -25,5 +26,12 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest authenticationRequest
     ){
         return authenticationService.authenticate(authenticationRequest);
+    }
+
+    private final UserService userService;
+
+    @GetMapping("/home")
+    public ResponseEntity<UserResponse> testHome(Authentication authentication) {
+        return userService.showUserDetails(authentication);
     }
 }
